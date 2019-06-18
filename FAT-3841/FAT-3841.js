dca09d437d35a12f3f5a8872816dd469217003ad
Anton Rozdobudko <arnygora@gmail.com>
2019-06-18 17:44:38 +0300

    delete short words from string

--- FAT-3841/FAT-3841.js
+++ FAT-3841/FAT-3841.js
@@ -1,22 +1,32 @@
 // 1. Дана строка символов. В самом длинном слове заменить все буквы «a» на «b».
-let string = "Lorem ipsum dolor sit amet adipisaaaicing elit facilis harum itaque nostrum quaerat saepe";
+const string = "Lorem ipsum dolor sit amet adipisaaaicing elit facilis harum itaque nostrum quaerat saepe";
 
 function findLongestWord(inputString) {
-    let filterArrayFromString = inputString.split(" ").sort((a, b) => b.length - a.length);
+    const filterArrayFromString = inputString.split(" ").sort((a, b) => b.length - a.length);
 
     function replaceLetterInWord() {
-        let longestWordFromArray = filterArrayFromString[0];
+        const longestWordFromArray = filterArrayFromString[0];
         return longestWordFromArray.replace(/a/g, 'b');
     }
     return replaceLetterInWord()
 }
+
 console.log(findLongestWord(string));
 
 // Определить, является ли строка палиндромом.
 function checkIsStringPalindrom(inputString) {
-    let reversedString = inputString.split('').reverse().join('');
+    const reversedString = inputString.split('').reverse().join('');
     return inputString === reversedString;
 }
 console.log(checkIsStringPalindrom('ene'));
 console.log(checkIsStringPalindrom('bene'));
 console.log(checkIsStringPalindrom('raba'));
+
+// 3. Удалить из строки все слова, длина которых меньше пяти символов. В строке не используются знаки препинания
+function deleteWords(string) {
+    const arrayFromString = string.split(" ");
+    const filteredArray = [];
+    filteredArray.push(...arrayFromString.filter((item) => item.length > 5));
+    return filteredArray.join(" ");
+}
+console.log(deleteWords(string));
