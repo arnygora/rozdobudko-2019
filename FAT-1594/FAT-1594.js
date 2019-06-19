980f673515a53a736f879ff1c15b400523e0ce9d
Anton Rozdobudko <arnygora@gmail.com>
2019-06-19 12:53:53 +0300

    used the method of the sum of an arbitrary number of brackets

--- FAT-1594/FAT-1594.js
+++ FAT-1594/FAT-1594.js
@@ -1,40 +1,39 @@
 // https://github.com/vvscode/js--interview-questions/blob/master/topics/closures.md
 // (fix with let variable)
-// for (let i = 0; i < 5; i++) {
-//     setTimeout(function () {
-//         console.log(i);
-//     }, 1000);
-// }
-//
-// // Create code for next conditions
-// // calculate('+')(1)(2); // 3
-// // calculate('*')(2)(3); // 6
-// function calculate(operator) {
-//     const operation = operator;
-//
-//     return function getFirstOperand(first) {
-//         const firstOperand = first;
-//
-//         return function getSecondOperand(second) {
-//             const secondOperand = second;
-//             let result = 0;
-//             switch (operation) {
-//                 case "+" :
-//                     result = firstOperand + secondOperand;
-//                     break;
-//                 case "*" :
-//                     result = firstOperand * secondOperand;
-//                     break;
-//                 default:
-//                     console.log(`${firstOperand}${operation}${secondOperand}`);
-//             }
-//             return result;
-//         }
-//     }
-// }
-//
-// console.log(calculate('+')(1)(2));
-// console.log(calculate('*')(2)(3));
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+
+// Create code for next conditions
+// calculate('+')(1)(2); // 3
+// calculate('*')(2)(3); // 6
+function calculate(operator) {
+    const operation = operator;
+
+    return function getFirstOperand(first) {
+        const firstOperand = first;
+
+        return function getSecondOperand(second) {
+            const secondOperand = second;
+            let result = 0;
+            switch (operation) {
+                case "+" :
+                    result = firstOperand + secondOperand;
+                    break;
+                case "*" :
+                    result = firstOperand * secondOperand;
+                    break;
+                default:
+                    console.log(`${firstOperand}${operation}${secondOperand}`);
+            }
+            return result;
+        }
+    }
+}
+console.log(calculate('+')(1)(2));
+console.log(calculate('*')(2)(3));
 
 // Create code for next conditions
 function sum(a) {
@@ -48,7 +47,6 @@ function sum(a) {
     };
     return showResult;
 }
-
 console.log(sum(1));
 console.log(sum(1)(2));
 console.log(sum(3)(4)(5));
