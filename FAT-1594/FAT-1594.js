c49a9bc6df23c17a410d6e1e42d4519de95f284b
Anton Rozdobudko <arnygora@gmail.com>
2019-06-19 10:34:16 +0300

    made with scope chaining

--- FAT-1594/FAT-1594.js
+++ FAT-1594/FAT-1594.js
@@ -5,3 +5,32 @@ for (let i = 0; i < 5; i++) {
         console.log(i);
     }, 1000);
 }
+
+// Create code for next conditions
+// calculate('+')(1)(2); // 3
+// calculate('*')(2)(3); // 6
+function calculate(operator) {
+    const operation = operator;
+
+    return function getFirstOperand(first) {
+        const firstOperand = first;
+        
+        return function getSecondOperand(second) {
+            const secondOperand = second;
+            let result = 0;
+            switch (operation) {
+                case "+" :
+                    result = firstOperand + secondOperand;
+                    break;
+                case "*" :
+                    result = firstOperand * secondOperand;
+                    break;
+                default:
+                    console.log(`${firstOperand}${operation}${secondOperand}`);
+            }
+            return result;
+        }
+    }
+}
+console.log(calculate('+')(1)(2));
+console.log(calculate('*')(2)(3));
