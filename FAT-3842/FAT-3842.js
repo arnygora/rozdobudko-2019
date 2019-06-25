363a354dc6da65a561df0466e82db3fcb0130d10
Anton Rozdobudko <arnygora@gmail.com>
2019-06-25 11:28:12 +0300

    check if array has repeated values

--- FAT-3842/FAT-3842.js
+++ FAT-3842/FAT-3842.js
@@ -1,6 +1,4 @@
-/* Дан одномерный массив. Найти среднее арифметическое его элементов. Вывести на экран только те элементы массива,
-которые больше найденного среднего арифметического. */
-const array = [15, 48, 46, 12, 33, 87, 545, 102, ];
+const array = [15, 48, 46, 12, 33, 87, 545, 102, 46];
 
 class WorkWithArray {
     constructor(array) {
@@ -16,7 +14,17 @@ class WorkWithArray {
     get valuesBiggerThanAverage() {
         return this.array.filter(item => item > this.averageArrayValue)
     }
+
+    get checkRepeatedValues() {
+        const sortedArray = array.sort();
+        return sortedArray.some((item, index, array) => {
+            // console.log(item[index] === item[index - 1], item[index]) // чому не працює з item ?
+            return (array[index] === array[index - 1])
+        })
+    }
 }
+
 const result = new WorkWithArray(array);
 console.log(result.averageArrayValue);
 console.log(result.valuesBiggerThanAverage);
+console.log(result.checkRepeatedValues);
